c7d167038a6903dbc951e2319a342c9f8ee5455f
Derek <dwilke311@gmail.com>
2026-03-26 11:25:19 -0400

    CleaningSupply.java
    Added Javadoc

--- CleaningSupply.java
+++ CleaningSupply.java
@@ -1,9 +1,32 @@
+/**
+ * CleaningSupply represents cleaning products sold in the store.
+ * It extends HouseholdItem and includes properties related to chemical safety.
+ */
 public class CleaningSupply extends HouseholdItem {
 
+    /** List of chemicals contained in the cleaning supply */
     private String[] chemicalsContained;
+
+    /** Indicates whether the product is toxic */
     private boolean toxic;
+
+    /** Indicates whether the product is flammable */
     private boolean flammable;
 
+    /**
+     * Creates a new CleaningSupply item.
+     *
+     * @param itemID Unique ID of the item
+     * @param name Name of the item
+     * @param price Price of the item
+     * @param quantity Quantity in inventory
+     * @param weight Weight of the item
+     * @param material Material of the container or product
+     * @param areaOfUse Area where the product is used
+     * @param chemicalsContained Array of chemicals in the product
+     * @param toxic Whether the product is toxic
+     * @param flammable Whether the product is flammable
+     */
     public CleaningSupply(int itemID, String name, double price, int quantity,
                           double weight, String material, String areaOfUse,
                           String[] chemicalsContained,
@@ -16,18 +39,44 @@ public class CleaningSupply extends HouseholdItem {
         this.flammable = flammable;
     }
 
+    /**
+     * Gets the chemicals contained in the cleaning supply.
+     * @return an array of chemical names
+     */
     public String[] getChemicalsContained(){ return chemicalsContained; }
+
+    /**
+     * Checks if the product is toxic.
+     * @return true if toxic, false otherwise
+     */
     public boolean isToxic(){ return toxic; }
+
+    /**
+     * Checks if the product is flammable.
+     * @return true if flammable, false otherwise
+     */
     public boolean isFlammable(){ return flammable; }
 
+    /**
+     * Sets the chemicals contained in the product.
+     * @param chemicalsContained array of chemical names
+     */
     public void setChemicalsContained(String[] chemicalsContained){
         this.chemicalsContained = chemicalsContained;
     }
 
+    /**
+     * Sets whether the product is toxic.
+     * @param toxic true if toxic, false otherwise
+     */
     public void setToxic(boolean toxic){
         this.toxic = toxic;
     }
 
+    /**
+     * Sets whether the product is flammable.
+     * @param flammable true if flammable, false otherwise
+     */
     public void setFlammable(boolean flammable){
         this.flammable = flammable;
     }
